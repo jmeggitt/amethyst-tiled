6a039e50219158ae31f06469ec13b6f2fca879a6
Jasper Meggitt <jasper.meggitt@gmail.com>
2019-11-26 12:14:28 -0500

    Replace error mod with failure

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,32 @@
-use tiled::parse_tileset;
-use amethyst::renderer::{SpriteSheet, Texture};
-use amethyst::renderer::rendy::texture::image::{load_from_image, ImageTextureConfig};
-use amethyst::assets::Loader;
 use amethyst::assets::AssetStorage;
+use amethyst::assets::Loader;
 use amethyst::assets::ProgressCounter;
+use amethyst::renderer::rendy::texture::image::{load_from_image, ImageTextureConfig};
 use amethyst::renderer::types::TextureData;
-use std::io::{Cursor, BufReader};
-use std::fs::File;
-use std::path::Path;
+use amethyst::renderer::{Sprite, SpriteSheet, Texture};
 use failure::Error;
+use std::fs::File;
+use std::io::{BufReader, Cursor};
+use std::path::Path;
+use tiled::parse_tileset;
 
 use sheep::SpriteSheet as PackedSpriteSheet;
+use sheep::{encode, AmethystFormat};
 
 mod set;
 
-
-pub fn load_tileset<P: AsRef<Path>>(path: P, loader: Loader, progress: &mut ProgressCounter, storage: &AssetStorage<Texture>) -> Result<PackedSpriteSheet, Error> {
+pub fn load_tileset<P: AsRef<Path>>(
+    path: P,
+    loader: Loader,
+    progress: &mut ProgressCounter,
+    storage: &AssetStorage<Texture>,
+) -> Result<PackedSpriteSheet, Error> {
     let tileset = parse_tileset(File::open(path)?, 1)?;
+    let tile_width = tileset.tile_width;
+    let tile_height = tileset.tile_height;
 
     let packed = set::find_then_pack(&tileset)?;
-
+    let (img_width, img_height) = packed.dimensions;
 
     let reader = BufReader::new(Cursor::new(packed.bytes));
 
@@ -27,6 +34,25 @@ pub fn load_tileset<P: AsRef<Path>>(path: P, loader: Loader, progress: &mut Prog
 
     let texture_handle = loader.load_from_data(TextureData(texture_builder), progress, storage);
 
+    let formatted = encode::<AmethystFormat>(&packed, ());
+
+    let sprites = formatted.sprites.iter().map(|x| Sprite::from_pixel_values(img_width, img_height, tile_width, tile_height, x.x as u32, x.y as u32, [0.0; 2], false, false)).collect();
+
+//    pub fn from_pixel_values(
+//        image_w: u32,
+//        image_h: u32,
+//        sprite_w: u32,
+//        sprite_h: u32,
+//        pixel_left: u32,
+//        pixel_top: u32,
+//        offsets: [f32; 2],
+//        flip_horizontal: bool,
+//        flip_vertical: bool,
+//    ) -> Sprite {
+//    let mut sprites = Vec::with_capacity(packed.)
+
+
+    ;
 
     unimplemented!()
 }
--- src/set.rs
+++ src/set.rs
@@ -1,9 +1,9 @@
 //! Module to help pack tile sets and convert them into amethyst
 //! https://github.com/amethyst/sheep/blob/master/sheep/examples/simple_pack/main.rs
-use image::{GenericImage, GenericImageView, ImageError};
+use failure::Error;
+use image::{GenericImage, GenericImageView, ImageError, Rgba};
 use sheep::{pack, InputSprite, SimplePacker, SpriteSheet};
 use tiled::Tileset;
-use failure::Error;
 
 pub fn find_then_pack(tiles: &Tileset) -> Result<SpriteSheet, Error> {
     let mut tile_bytes = Vec::with_capacity(tiles.tiles.len());
@@ -12,8 +12,14 @@ pub fn find_then_pack(tiles: &Tileset) -> Result<SpriteSheet, Error> {
         let mut img_src = image::open(&img.source)?;
         let img_src = match img_src.as_mut_rgba8() {
             Some(v) => v,
-            None => return Err(ImageError::FormatError("Unable to read non rgba8 images".to_owned()).into()),
+            None => {
+                return Err(
+                    ImageError::FormatError("Unable to read non rgba8 images".to_owned()).into(),
+                )
+            }
         };
+
+//        img_src.repl
 
         for x in (tiles.margin..img.width as u32 - tiles.margin)
             .step_by((tiles.tile_width + tiles.spacing) as usize)
@@ -21,12 +27,24 @@ pub fn find_then_pack(tiles: &Tileset) -> Result<SpriteSheet, Error> {
             for y in (tiles.margin..img.height as u32 - tiles.margin)
                 .step_by((tiles.tile_height + tiles.spacing) as usize)
             {
-                let tile_pixels = img_src
+                let mut tile_pixels = img_src
                     .sub_image(x, y, tiles.tile_width, tiles.tile_height)
                     .pixels()
-                    .map(|it| it.2 .0.to_vec())
-                    .flat_map(|x| x)
-                    .collect();
+                    .map(|x| x.2);
+
+                if let Some(color) = img.transparent_colour {
+                    tile_pixels = tile_pixels.map(|x| {
+                        let mut Rgba([r, g, b, a]) = x;
+                        if color.red == r && color.green == g && color.blue == b {
+                            a = 0xFF;
+                        }
+                        [r, g, b, a]
+                    })
+                }
+
+                    let tile_pixels = tile_pixels
+                        .flat_map(|x| x.to_vec())
+                        .collect();
 
                 let sprite = InputSprite {
                     dimensions: (tiles.tile_width, tiles.tile_height),
